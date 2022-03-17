2be0923575ee4d76b30c1835a1e03bc1c1347bf0
TimmyTheRenaissanceMan <ahmetov.timur95@gmail.com>
2022-03-17 23:57:27 +0100

    add ticker

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+<p class="ticker">
+  <span>Ticker - Ticker - Ticker - Ticker - Ticker - Ticker - Ticker - Ticker - Ticker - Ticker - Ticker - Ticker - Ticker - Ticker - Ticker - Ticker - Ticker - Ticker - Ticker - Ticker - Ticker - Ticker - Ticker - Ticker - Ticker - Ticker - Ticker - Ticker - Ticker - Ticker - Ticker - Ticker - Ticker - Ticker - Ticker - Ticker -&nbsp;</span>
+</p>
+<p class="ticker ticker2">
+  <span>Ticker - Ticker - Ticker - Ticker - Ticker - Ticker - Ticker - Ticker - Ticker - Ticker - Ticker - Ticker - Ticker - Ticker - Ticker - Ticker - Ticker - Ticker - Ticker - Ticker - Ticker - Ticker - Ticker - Ticker - Ticker - Ticker - Ticker - Ticker - Ticker - Ticker - Ticker - Ticker - Ticker - Ticker - Ticker - Ticker -&nbsp;</span>
+</p>
+    </div>
+  );
+}
+
+export default App;
